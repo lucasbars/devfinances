ad373e4acfa2c556f7d2d030f1d6f8a0e0f69873
lucas <lucas@lucas.com>
2022-08-05 09:32:26 -0300

    display app

--- scripts.js
+++ scripts.js
@@ -11,13 +11,13 @@ const transactions = [
   {
     id: 1,
     description: 'Luz',
-    amount: -50000,
+    amount: -50001,
     date: '23/01/2020'
   },
   {
     id: 2,
     description: 'Website',
-    amount: 500000,
+    amount: 500020,
     date: '23/01/2020'
   },
   {
@@ -30,19 +30,35 @@ const transactions = [
 
 const Transaction = {
   incomes() {
-
+    let income = 0;
+    transactions.forEach((transaction)=>{
+      if(transaction.amount > 0 ){
+        income += transaction.amount;
+      }
+    });
+    return income;
   },
   expenses() {
-
+    let expense = 0;
+    transactions.forEach((transaction)=>{
+      if(transaction.amount < 0 ){
+        expense += transaction.amount;
+      }
+    });
+    return expense;
   },
   total() {
-
+    return Transaction.incomes() + Transaction.expenses();
   }
 }
 
 const Utils = {
   formatCurrency(value){
     const signal = Number(value) < 0 ? "-" : "";
+    value = String(value).replace(/\D/g,"");
+    value =Number(value) / 100;
+    value = value.toLocaleString("pt-BR",{style:"currency",currency:"BRL"})
+    return signal + value;
     
   }
 }
@@ -64,15 +80,23 @@ const DOM = {
 
     const html = `
       <td class="description">${transaction.description}</td>
-      <td class="${CSSclass}">${transaction.amount}</td>
+      <td class="${CSSclass}">${amount}</td>
       <td class="date">${transaction.date}</td>
       <td><img src="./assets/minus.svg" alt ="Remover Transacao"></td>
     `;
     return html;
   },
-}
 
+  upadateBalance(){
+    document.getElementById('incomeDisplay').innerHTML=Utils.formatCurrency(Transaction.incomes());
+    document.getElementById('expenseDisplay').innerHTML=Utils.formatCurrency(Transaction.expenses());
+    document.getElementById('totalDisplay').innerHTML=Utils.formatCurrency(Transaction.total());
+
+  },
+}
 
 transactions.forEach(function(transaction){
   DOM.addTransaction(transaction)
-})
+});
+
+DOM.upadateBalance();
